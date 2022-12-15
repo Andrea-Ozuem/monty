ce6f988dac2f4547621b74ea2ae5364685836eda
LordZeuz <chriskwesi12@gmail.com>
2022-12-15 03:33:00 +0000

    test

---
+++ bytecodes/07.m
@@ -0,0 +1,13 @@
+push 1
+push 2
+push 3
+push 4
+pall
+pint
+nop
+mul
+div
+mod
+print
+push 0
+mod
---
+++ bytecodes/08.m
@@ -0,0 +1,17 @@
+push 1
+push 2
+push 3
+push 4
+pall
+pint
+nop
+pall
+mul
+pall
+div
+pall
+mod
+pall
+print
+push 0
+mod
